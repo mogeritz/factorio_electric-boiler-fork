2be86e3d59a59541d1bbd0d8a54522eb64490bd6
mogeritz <moge.world.kanal@gmail.com>
2022-10-10 00:07:03 -0700

    Improved technology icon (128px, real graphic)

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -2,8 +2,8 @@ local technology_boiler = table.deepcopy(data.raw["technology"]["automation"])
 local technology_override = {
   name = "electric-boiler",
   prerequisites = {"automation"},
-  icon = "__electricboiler_mogeritz__/graphics/icons/electric-boiler.png",
-  icon_size = 32,
+  icon = "__electricboiler_mogeritz__/graphics/icons/technology/electricboiler.png",
+  icon_size = 128,
   effects = {
     {
       type = "unlock-recipe",
